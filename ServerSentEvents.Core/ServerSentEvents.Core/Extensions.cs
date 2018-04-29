4ed8f29bf88e1a7a9dca93cb2672e56f7392a49f
hakansoysal <hakan.soysal@aurorabilisim.com>
2018-04-29 15:41:30 +0300

    Extensins Comments added

--- ServerSentEvents.Core/ServerSentEvents.Core/Extensions.cs
+++ ServerSentEvents.Core/ServerSentEvents.Core/Extensions.cs
@@ -9,13 +9,24 @@ namespace ServerSentEvents.Core
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Register SSE Service as Singleton
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="isSubscription">Predicate for Request is a SSE registration</param>
+        /// <param name="getTopicName">Get TopicName funcion</param>
+        /// <returns></returns>
         public static IServiceCollection AddServerSentEvents(this IServiceCollection services, Func<HttpContext, bool> isSubscription, Func<HttpContext, string> getTopicName)
         {
             services.AddSingleton<ServerSentEventsService>(new ServerSentEventsService(isSubscription, getTopicName));
             return services;
         }
 
-
+        /// <summary>
+        /// Register SSE Middleware
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
         public static IApplicationBuilder UseServerSentEvents(this IApplicationBuilder app)
         {
             return app.UseMiddleware<ServerSentEventsMiddleware>();
